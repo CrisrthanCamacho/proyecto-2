bcff21946c7a17c6b6b4f14a98b0aa5449277a0e
cristhian camacho <crisrthiancamacho@gmail.com>
2025-11-24 23:14:38 -0500

    menu actualizado

--- menu.py
+++ menu.py
@@ -2,48 +2,52 @@ from pedidos import GestorPedidos
 gestor_pedidos = GestorPedidos()
 from prueba import GestionConfeccionistas
 gestion_confeccionistas = GestionConfeccionistas()
+import usuarios
+
+usuario_actual = None
+
 # menú inicio de sesión
-
-
 def mostrar_menu_principal():
+    global usuario_actual
+
     print("\n===== MENÚ PRINCIPAL =====")
-    print("1. Registrarme como Admin")
-    print("2. Registrarme como Cliente")
-    print("3. Iniciar Sesión")
-    print("4. Salir")
+    print("1. Registrarme")
+    print("2. Iniciar Sesión")
+    print("3. Salir")
 
 #------------------------------------------------------------
 
 
 # menu del cliente
-
-
 def menu_cliente():
     while True:
         print("\n===== MENÚ CLIENTE =====")
         print("1. editar perfil")
-        print("2. registrar pedido")
-        print("3. historial de pedidos")
-        print("4. abonar pedido")
-        print("5. cancelar pedido")
-        print("6. pagar pedido")
-        print("7. Cerrar Sesión")
+        print("2. info de perfil")
+        print("3. registrar pedido")
+        print("4. historial de pedidos")
+        print("5. pagar pedido")
+        print("6. anticipos de pedido")
+        print("7. cancelar pedido")
+        print("8. Cerrar Sesión")
 
         opcion = input("Elige una opción: ")
         if opcion == "1":
-            print("")
+            usuarios.editar_perfil(usuario_actual)
         elif opcion =="2":
-            gestor_pedidos.agregar_pedido()
+            usuarios.ver_perfil(usuario_actual)
         elif opcion =="3":
+            gestor_pedidos.agregar_pedido(usuario_actual)
+        elif opcion =="4":
             gestor_pedidos.mostrar_pedidos()
-        elif opcion == "4":
-            print("s")
         elif opcion == "5":
-            gestor_pedidos.cancelar_pedido()
+            gestor_pedidos.pagar_pedido()
         elif opcion == "6":
-            print("s")
-            
+            gestor_pedidos.mostrar_anticipos_pedido()
         elif opcion == "7":
+            gestor_pedidos.cancelar_pedido()
+            
+        elif opcion == "8":
             print("Cerrando sesión...")
             break
         else:
@@ -53,14 +57,15 @@ def menu_cliente():
 
 
 # menu de admin
-
 def menu_admin():
     while True:
         print("\n===== MENÚ ADMINISTRADOR =====")
         print("1. gestion de confeccionistas")
         print("2. gestion de pedidos")
-        print("3. editar mi info")
-        print("4. cerrar sesión")
+        print("3. ver clientes registrados")
+        print("4. editar mi info")
+        print("5. info de perfil")
+        print("6. cerrar sesión")
 
         opcion = input("Elige una opción: ")
         
@@ -69,8 +74,12 @@ def menu_admin():
         elif opcion =="2":
             menu_gestion_pedidos()
         elif opcion =="3":
-            print("")
-        elif opcion == "4":
+            usuarios.ver_clientes()
+        elif opcion =="4":
+            usuarios.editar_perfil(usuario_actual)
+        elif opcion =="5":
+            usuarios.ver_perfil(usuario_actual)
+        elif opcion == "6":
             print("Cerrando sesión de administrador...")
             break
         else:
@@ -159,61 +168,67 @@ def menu_gestion_pedidos():
 
 #------------------------------------------------------------------------------------
 
-# registro (prueba)
-
-
-def menu_registro_usuario():
-    print("\n--- Registro de Usuario/Admin ---")
 
 
 
-def menu_registro_cliente():
-    print("\n--- Registro de Cliente ---")
+def menu_registro():
+    print("\n--- Registro ---")
+    print("1. Registrarme como cliente")
+    print("2. Registrarme como admin")
+    opcion= (input("elige una opcion"))
+    if opcion == "1":
+        usuarios.registrar_cliente()
+        
+
+    elif opcion == "2":
+        usuarios.registrar_admin()
+        
+    else:
+        print("opcion invalida")
 
 
 
 
 # inicio de sesión (prueba)
-
-
 def menu_inicio_sesion():
+    global usuario_actual
     print("\n--- Iniciar Sesión ---")
     print("1. iniciar sesión como cliente")
     print("2. iniciar sesión como admin")
     opcion= (input("elige una opcion"))
 
     if opcion == "1":
-        print("s")
-        menu_cliente()
+        usuario_actual = usuarios.iniciar_sesion(usuarios.clientes)
+        if usuario_actual:
+            menu_cliente()
 
     elif opcion == "2":
-        print("")
-        menu_admin()
+        usuario_actual = usuarios.iniciar_sesion(usuarios.administradores)
+        if usuario_actual:
+            menu_admin()
     else:
-        print("Tipo de usuario desconocido.")
+        print("opcion invalida")
 
+#------------------------------------------------------------------------------------
 
-
+#menu de registro o inicio de sesión
 def main():
     while True:
         mostrar_menu_principal()
         opcion = input("Selecciona una opción: ")
 
         if opcion == "1":
-            menu_registro_usuario()
+            menu_registro()
 
         elif opcion == "2":
-            menu_registro_cliente()
-
-        elif opcion == "3":
             menu_inicio_sesion()
 
-        elif opcion == "4":
+        elif opcion == "3":
             print("Saliendo del sistema...")
             break
 
         else:
-            print("Opción no válida. Intenta de nuevo.")
+            print("Opción invalida")
 
 
 
